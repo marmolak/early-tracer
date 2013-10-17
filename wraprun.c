c7385c8a27397ab245b0b9d4216123e811eef168
Robin Hack <rhack@redhat.com>
2013-10-17 19:10:44 +0200

    Added proc interface for wrapper.

--- wraprun.c
+++ wraprun.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <stdio.h>
 
-
+/* Because pause() */
 void unfreeze (int unused)
 {
     return;
@@ -15,6 +19,14 @@ int main (int argc, char **argv)
     char **new_argv = NULL;
     struct sigaction sig;
     memset (&sig, 0, sizeof(sig));
+
+    int fd = open ("/proc/systemtap/attach_gdb/wrapper_path", O_WRONLY);
+    if ( fd == -1 ) {
+        perror ("Open file");
+        return EXIT_FAILURE;
+    }
+    write (fd, argv[0], strlen(argv[0]));
+    close (fd);
 
     sig.sa_handler = &unfreeze;
     sigaction (SIGUSR1, &sig, NULL);
